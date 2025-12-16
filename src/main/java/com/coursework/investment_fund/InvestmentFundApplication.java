56c31c4294a258cf3dfea23d9c6baaeb72ba4bdb
Renat Iunisov <ryunisov0@gmail.com>
2025-12-16 16:52:01 +0300

    #1 Добавление новых файлов

---
+++ src/main/java/com/coursework/investment_fund/InvestmentFundApplication.java
@@ -0,0 +1,13 @@
+package com.coursework.investment_fund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentFundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentFundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coursework/investment_fund/InvestmentFundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursework.investment_fund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentFundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
